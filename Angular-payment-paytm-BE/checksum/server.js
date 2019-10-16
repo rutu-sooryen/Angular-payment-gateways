280adaad5b3cbac32ebde5a04c88202a452cc25b
sooryen <sooryen@DESKTOP-ERPBQJD>
2019-10-16 17:52:41 +0530

    Change port number from 8080 to 3000

--- Angular-payment-paytm-BE/checksum/server.js
+++ Angular-payment-paytm-BE/checksum/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const https = require('https');
 const qs = require('querystring');
-const port = 8080;
+const port = 3000;
 const checksum_lib = require('./checksum.js');
 
 var PaytmConfig = {
