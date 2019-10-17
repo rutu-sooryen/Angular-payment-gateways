e7b989f5727d07db7c5b97cac060f17f03708658
sooryen <sooryen@DESKTOP-ERPBQJD>
2019-10-17 18:24:02 +0530

    Call Backend node API anf generate checksum

--- Angular-payment-paytm/src/app/app.component.ts
+++ Angular-payment-paytm/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularPaymentPaytmService } from './services/angular-payment-paytm.service';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-payment-paytm';
-
-  RequestAttributes = {
-      MID : 'xOpHGh95840523930721',
-      ORDER_ID : 'OrderId@1234567890',
-      CUST_ID : 'CustId@1234567890',
-      TXN_AMOUNT : '1000',
-      CHANNEL_ID : 'WEB',
-      MOBILE_NUMBER : '1234567890',
-      EMAIL : 'r.d@abc.com',
-      WEBSITE : 'WEBSTAGING',
-      CHECKSUMHASH : '',
-      INDUSTRY_TYPE_ID : 'Retail',
-      CALLBACK_URL : 'https://merchant.com/callback/'
+  public requestAttributes: {
+    MID: string,
+    ORDER_ID: string,
+    CUST_ID: string,
+    TXN_AMOUNT: string,
+    CHANNEL_ID: string,
+    MOBILE_NUMBER: string,
+    EMAIL: string,
+    WEBSITE: string,
+    CHECKSUMHASH: string,
+    INDUSTRY_TYPE_ID: string,
+    CALLBACK_URL: string
+  };
+  constructor(
+    public paytmPaymentService: AngularPaymentPaytmService) { }
+  ngOnInit() {
+    console.log("onInit");
+    this.paytmPaymentService.getChecksum().subscribe((checksum: any) => {
+      console.log("checksum");
+      this.requestAttributes = {
+        MID: 'xOpHGh95840523930721',
+        ORDER_ID: 'OrderId@1234567890',
+        CUST_ID: 'CustId@1234567890',
+        TXN_AMOUNT: '1000',
+        CHANNEL_ID: 'WEB',
+        MOBILE_NUMBER: '1234567890',
+        EMAIL: 'r.d@abc.com',
+        WEBSITE: 'WEBSTAGING',
+        CHECKSUMHASH: checksum,
+        INDUSTRY_TYPE_ID: 'Retail',
+        CALLBACK_URL: 'https://merchant.com/callback/'
+      }
+    });
   }
+
+
+
 }
