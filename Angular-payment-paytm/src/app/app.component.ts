03a75d49dc0ec996a4c991502f2eeedc742554ec
sooryen <sooryen@DESKTOP-ERPBQJD>
2019-10-21 19:41:47 +0530

    change passing attributes

--- Angular-payment-paytm/src/app/app.component.ts
+++ Angular-payment-paytm/src/app/app.component.ts
@@ -8,41 +8,23 @@ import { AngularPaymentPaytmService } from './services/angular-payment-paytm.ser
 })
 export class AppComponent {
   title = 'Angular-payment-paytm';
-  public requestAttributes: {
-    MID: string,
-    ORDER_ID: string,
-    CUST_ID: string,
-    TXN_AMOUNT: string,
-    CHANNEL_ID: string,
-    MOBILE_NUMBER: string,
-    EMAIL: string,
-    WEBSITE: string,
-    CHECKSUMHASH: string,
-    INDUSTRY_TYPE_ID: string,
-    CALLBACK_URL: string
+  public requestAttributes: any = {
+    MID: 'xOpHGh95840523930721',
+    ORDER_ID: 'OrderId@1234567890',
+    CUST_ID: 'CustId@1234567890',
+    TXN_AMOUNT: '001.00',
+    CHANNEL_ID: 'WEB',
+    WEBSITE: 'WEBSTAGING',
+    INDUSTRY_TYPE_ID: 'Retail',
+    CALLBACK_URL: 'http://localhost:4200/callback'
   };
   constructor(
     public paytmPaymentService: AngularPaymentPaytmService) { }
   ngOnInit() {
-    console.log("onInit");
+
     this.paytmPaymentService.getChecksum().subscribe((checksum: any) => {
       console.log("checksum");
-      this.requestAttributes = {
-        MID: 'xOpHGh95840523930721',
-        ORDER_ID: 'OrderId@1234567890',
-        CUST_ID: 'CustId@1234567890',
-        TXN_AMOUNT: '1000',
-        CHANNEL_ID: 'WEB',
-        MOBILE_NUMBER: '1234567890',
-        EMAIL: 'r.d@abc.com',
-        WEBSITE: 'WEBSTAGING',
-        CHECKSUMHASH: checksum,
-        INDUSTRY_TYPE_ID: 'Retail',
-        CALLBACK_URL: 'https://merchant.com/callback/'
-      }
+      this.requestAttributes.CHECKSUMHASH = checksum;
     });
   }
-
-
-
 }
